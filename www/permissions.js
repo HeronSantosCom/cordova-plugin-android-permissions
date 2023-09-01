5ce23e99ddb4d35ba2ed05db01e2d35c0b8a5828
Stefan Saenger <stefan.saenger@vnc.biz>
2023-09-01 13:03:53 +0200

    fix: permission list

--- www/permissions.js
+++ www/permissions.js
@@ -101,6 +101,9 @@ function Permissions() {
     this.READ_HISTORY_BOOKMARKS = 'com.android.browser.permission.READ_HISTORY_BOOKMARKS';
     this.READ_INPUT_STATE = 'android.permission.READ_INPUT_STATE';
     this.READ_LOGS = 'android.permission.READ_LOGS';
+    this.READ_MEDIA_AUDIO = 'android.permission.READ_MEDIA_AUDIO';
+    this.READ_MEDIA_IMAGES = 'android.permission.READ_MEDIA_IMAGES';
+    this.READ_MEDIA_VIDEO = 'android.permission.READ_MEDIA_VIDEO';	
     this.READ_PHONE_STATE = 'android.permission.READ_PHONE_STATE';
     this.READ_PROFILE = 'android.permission.READ_PROFILE';
     this.READ_SMS = 'android.permission.READ_SMS';
